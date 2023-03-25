50f08e27efa334fa8bb026d51f3bc2d335f5a300
f.cage.f <f.cage.f@foxmail.com>
2023-03-25 10:12:08 +0800

    添加了预处理减少压缩次数

--- main.py
+++ main.py
@@ -5,16 +5,33 @@ import threading
 
 def compress(file, size_limit=2.0, rate=0.95):
     size = os.path.getsize(file)/1024/1024
+    if size <= size_limit:
+        return
+    
+    # 加一步预处理，使图片大小在2左右，减少压缩次数
+    r = (size_limit / size) ** 0.5
+    img = Image.open(file)
+    x, y = img.size
+    img.resize([int(x*r), int(y*r)], Image.LANCZOS).save(file)
+    size = os.path.getsize(file)/1024/1024
+    # print(file, size,sep='\t')
+
     while size > size_limit:
         img = Image.open(file)
         x, y = img.size
         img.resize([int(x*rate), int(y*rate)], Image.LANCZOS).save(file)
         size = os.path.getsize(file)/1024/1024
         # print('\r', file, size, end='')
-    print(file, size, 'MB')
+        # print('\t', size,sep='\t')
+    # print()
+    print(f'{file}\t{size}MB')
 
 
 dir = './images'
+
+# for file in os.listdir(dir):
+#     compress(os.path.join(dir, file))
+
 
 threads = []
 for file in os.listdir(dir):
